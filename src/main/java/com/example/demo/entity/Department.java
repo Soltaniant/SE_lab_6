6191047a4a53ee59bed9f091d64f61d85f03ddc3
s.soltanian <s.soltaniant@gmail.com>
2024-12-06 09:47:59 +0330

    add initial CRUD web service

---
+++ src/main/java/com/example/demo/entity/Department.java
@@ -0,0 +1,53 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long departmentId;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public String getDepartmentAddress() {
+        return departmentAddress;
+    }
+
+    public String getDepartmentCode() {
+        return departmentCode;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentAddress(String departmentAddress) {
+        this.departmentAddress = departmentAddress;
+    }
+
+    public void setDepartmentCode(String departmentCode) {
+        this.departmentCode = departmentCode;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+}
